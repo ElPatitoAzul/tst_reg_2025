97fed7235cbf759444ebaa49592ec83f52bac817
KEVDEV <elpatitoazul2023@gmail.com>
2025-05-27 07:23:59 -0600

    Descripción de los cambios que hiciste

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,25 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'Splashscreen/Splashscreen.dart';
-import 'form/Register.dart';
-
 void main() {
   runApp(const MyApp());
-  HttpOverrides.global = MyHttpOverrides();
-}
-
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.hbjhgjhgjhj
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nuevo Proyecto',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Spashscreen(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Bienvenido a register_tst_2025_nuevo'),
+        ),
+      ),
     );
   }
 }
